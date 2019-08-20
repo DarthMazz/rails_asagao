5451719a16b6f245ba1ac8b2ece8e5f715288bd7
DarthMazz <yo4taka.ma2moto@gmail.com>
2019-08-20 17:07:11 +0900

    validationを追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,11 +8,12 @@ class Member < ApplicationRecord
     },
     uniqueness: true
   validates :name, presence: true,
-    format: {with:^A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true},
-    lenght: {mininum:2, maximum: 20, allow_blank: true},
-    uniqueness: {case_sensitive: false}
-  validates :full_name, presence: true, length: {maxinum: 20}
-  
+    format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true },
+    length: { minimum:2, maximum: 20, allow_blank: true },
+    uniqueness: { case_sensitive: false }
+  validates :full_name, presence: true, length: { maximum: 20 }
+  validates :email, email: { allow_blank: true }
+
   class << self
     def search(query)
       rel = order("number")
